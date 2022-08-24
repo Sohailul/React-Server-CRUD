563797ddd32b9e5103d6acc39e5259680ccbd308
Sohailul Alam <sohailulalam@gmail.com>
2022-08-24 15:45:31 +0600

    Done

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbuser1:8p4ZpzUwRWOf3Ric@cluster0.xfasllp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("DataDB").collection("users");
+    // get users
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    //Add User
+    app.post("/user", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running my node server");
+});
+
+app.listen(port, () => {
+  console.log(`CRUD Server is Running`);
+});
